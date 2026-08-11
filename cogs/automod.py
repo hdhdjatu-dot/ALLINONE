b86716ee3a84f629cf0e131351c31807d99d4886
hdhdjatu-dot <hdhdjatu@gmail.com>
2026-08-11 16:44:39 +0530

    automod.py

--- cogs/automod.py
+++ cogs/automod.py
@@ -40,7 +40,12 @@ class AutoMod(commands.Cog):
             "mc", "randi", "maderchod", "chakka", "bhenchod",
             "bhosdika", "chutiye", "bsdk", "gand", "gand mara",
             "muh me lele", "teri maa chod dunga", "tun chakka hai",
-            "bc", "bhosdike", "chutiya", "lodu", "bkl"
+            "bc", "bhosdike", "chutiya", "lodu", "bkl" ,"bhen ka loda" "maa ka bhosda" ,"chut"  ,"bhund", "fudda" ,"sex" ,"radn" ,"randdd"
+
+
+
+
+
         }
 
         # Limits
